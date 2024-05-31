cc886a16c99777a3d7580c9e0237253dc1abcae2
abhishekbhonde <abhishekbhonde567@gmail.com>
2024-05-31 18:53:12 +0530

    category route added successfully

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const userRouter = require('./routes/User')
 const productRouter = require('./routes/Product')
+const categoryRouter = require('./routes/Category');
 
 
 app.use(bodyParser.json());
 
 app.use('/user', userRouter);
 app.use('/product', productRouter)
+app.user('/category', categoryRouter);
 
 
 
---
+++ apps/backend/src/routes/Category.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { Router } = require("express");
+const {Category} = require('../db/index')
+const router = Router();
+const app = express();
+app.use(experss.json());
+
+
+router.post('/Items', (req,res)=>{
+    const {categoryId, categoryName,description} = req.body;
+    try {
+        const category = Category.create({
+            categoryId,
+            categoryName,
+            description
+        })
+    if(category){
+        res.json({
+            msg:"Category added successfully"
+        })
+   
+    } else{
+        res.status(500).json({
+            msg:"Some error occured"
+        })
+    }
+    } catch (error) {
+        res.status(500).json({
+            msg:"error adding category",
+            error:error.message
+        })
+    }
+})
+
+
+
+module.exports = router
